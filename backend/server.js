ae6a74fcb6a45d424e60efb1cfbf39aaa3fb3d41
Abhishekiitb25 <jainabhi233@gmail.com>
2024-07-15 20:50:17 +0530

    Phase-4 Done Socket.IO

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -6,9 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -25,7 +25,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello World!!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,6 +3,7 @@ import useConversation from "../../zustand/useConversation";
 import Messages from "./Messages";
 import MessagesInput from "./MessagesInput";
 import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
@@ -35,10 +36,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>welcome 👋 John 🌻</p>
+        <p>welcome 👋 {authUser.fullName} 🌻</p>
         <p>Start a conversation by selecting a chat</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
